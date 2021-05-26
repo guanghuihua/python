6ed43604436c2bd5d6bceb334c2c06072c6d8996
Guanghui-hua <15890947916@163.com>
2021-05-26 19:14:57 +0800

    clear the space

--- np的一些用法.py
+++
@@ -1,8 +0,0 @@
-#-------------np.where()的一些用法
-#np.where(condition,a,b)  -->如果条件是真的，那么就是a,否则就是b
-import numpy as np
-X = np.arange(9).reshape(3,3)
-Y = np.ones((3,3))
-print(np.where(X > 5,X,Y))
-
-print(np.where([[True,False],[True,True]],[[1,2],[3,4]],[[5,6],[7,8]]))
