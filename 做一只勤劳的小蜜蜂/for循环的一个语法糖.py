f0569357078ca26d7666c3f4cf23938cb4d86ab5
Guanghui-hua <15890947916@163.com>
2021-06-15 16:06:34 +0800

    for循环的一个语法糖

---
+++ 做一只勤劳的小蜜蜂/for循环的一个语法糖.py
@@ -0,0 +1,16 @@
+# 最近在完成大作业的时候发现一个for循环的一个非常好用的例子
+
+
+for _ in range(10):
+    print("Hello world!")
+
+# Hello world!
+# Hello world!
+# Hello world!
+# Hello world!
+# Hello world!
+# Hello world!
+# Hello world!
+# Hello world!
+# Hello world!
+# Hello world!
