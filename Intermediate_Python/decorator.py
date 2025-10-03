41904614fce59ec8bc35500a7353a5202ebd1899
guanghuihua <hua17066003@gmail.com>
2025-10-03 18:20:20 +0800

    add README_cpp.md

--- Intermediate_Python/decorator.py
+++ Intermediate_Python/decorator.py
@@ -1,14 +1,14 @@
-from functools import wraps
-
 def log(func):
     def wrapper(*args, **kw):
-        print(f"call {func.__name__}")
+        print("call %s ()" % func.__name__)
         return func(*args, **kw)
     return wrapper
 
 @log
 def now():
-    print("2025-10-02")
+    print("===10.3===")
+    pass
+
 
 if __name__ == "__main__":
     now()
