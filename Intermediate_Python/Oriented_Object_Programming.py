49854723b2e0e8a8cb3ef790dbe28fa86410d36a
guanghuihua <hua17066003@gmail.com>
2025-10-10 15:10:35 +0800

    revisited OOP

---
+++ Intermediate_Python/Oriented_Object_Programming.py
@@ -0,0 +1,11 @@
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    
+    def print_score(self):
+        print('%s : %s' %(self.name, self.score))
+
+if __name__ == "__main__":
+    bart = Student('Bart Simpon', 59)
+    bart.print_score()
